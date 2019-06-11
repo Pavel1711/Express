15d62f7517a4555f2b61c6e520f32ca8750f9de6
Pavel1711 <45904265+Pavel1711@users.noreply.github.com>
2019-06-11 08:38:34 +0300

    Добавил исключения и двойную кодировку пароля в register.php

--- register.php
+++ register.php
@@ -1,15 +1,29 @@
-<?
+<?  
+if(!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['email']) && !empty($_POST['password'])){
+
+    if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['email']))
+    {
+        echo("-1");
+    }else if(strlen($_POST['email']) < 3 or strlen($_POST['email']) > 30)
+    {
+        echo("-2");
+    }else{
+    mysql_connect("localhost","root","") or die ("Невозможно подключиться к серверу");
+    mysql_select_db("express") or die ("Нет такой таблицы");
+
     $name = $_POST['name'];
     $surname = $_POST['surname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    mysql_connect("localhost","root","") or die ("Невозможно подключиться к серверу");
-    mysql_select_db("express") or die ("Нет такой таблицы");
+    $password = md5(md5(trim($_POST['password'])));
+
     $auth = mysql_query("SELECT *FROM profile WHERE email='$email'");
-    if(mysql_num_rows($auth)==1){
-        echo(false);
-    }else{
-        $auth = mysql_query("INSERT INTO `profile`(`Email`, `Password`, `Name`, `Surname`) VALUES ('$email','$password','$name','$surname')");
-        echo(true);
+        if(mysql_num_rows($auth)==1){
+            echo("0");
+        }else{
+            $auth = mysql_query("INSERT INTO `profile`(`Email`, `Password`, `Name`, `Surname`) VALUES ('$email','$password','$name','$surname')");
+            echo("1");
+        }
     }
+}
 ?>
